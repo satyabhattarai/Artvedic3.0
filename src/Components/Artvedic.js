3e0d1b7d59d173a60b23eaf6be861ade123974e8
satyabhattarai <satyabhattarai.055@gmail.com>
2025-08-17 20:20:57 +0545

    Artvedic-page

---
+++ src/Components/Artvedic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Artvedic = () => {
+  return (
+    <div>
+      <div className="bg-white text-primary font-brand font-medium text-[48px] flex  justify-center w-full border-gray-300">
+        artvedic
+      </div>
+    </div>
+  );
+};
+
+export default Artvedic;
